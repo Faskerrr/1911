eafd089642b6ecf20d6e8a0b439a59015ea7b956
Vlad Drozhzhin <Faskerrr@gmail.com>
2025-01-24 13:26:11 +0400

    streamlit push

--- app.py
+++ app.py
@@ -4,3 +4,9 @@ st.write('Welcome to my app')
 
 
 st.markdown('# Batch 1911 is hosting this one')
+
+spell = st.secrets['spell']
+key = st.secrets.some_magic_api.key
+
+st.write(spell)
+st.write(key)
